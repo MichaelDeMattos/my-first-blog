cb046984cc8055c591738f04cec39450ff48ed4c
Michael de Mattos <chelmto3000@gmail.com>
2020-01-11 09:22:02 -0300

    Include Profile

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+# cria uma função que vai fazer uma requisição e renderizar nossa pagina main
+def post_list(request):
+    return render(request, 'blog/post_list.html',
+                  {})
