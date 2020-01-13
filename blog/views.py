9558ff23b8b5988e303b0e0b99696af140e8c1e2
Michael de Mattos <chelmto3000@gmail.com>
2020-01-12 21:06:27 -0300

    Update Post Blog

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 
 # Create your views here.
 
 # cria uma função que vai fazer uma requisição e renderizar nossa pagina main
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now())
     return render(request, 'blog/post_list.html',
-                  {})
+                  {'posts': posts})
 
 
 def sobre_dev(request):
     return render(request, 'blog/sobre.html',
                   {})
+
+
