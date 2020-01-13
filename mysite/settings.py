10662715a409eab513a1de557f0c4d1312ac4dda
Michael de Mattos <chelmto3000@gmail.com>
2020-01-13 19:35:31 -0300

    Update CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,5 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = (
+   '/blog/templates/blog/static',
+)
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Additional locations of static files
